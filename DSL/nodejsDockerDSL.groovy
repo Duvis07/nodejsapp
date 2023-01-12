5970ec8a4c9a1afd554a6fe9954b9617020ac56b
Duvan Arley Botero <96325513+Duvis07@users.noreply.github.com>
2023-01-12 09:43:22 -0500

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -15,7 +15,6 @@ job('Aplicacion Node.js Docker DSL Docker') {
     steps {
         dockerBuildAndPublish {
             repositoryName('duvis07/nodejsapp')
-            tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
             createFingerprints(false)
